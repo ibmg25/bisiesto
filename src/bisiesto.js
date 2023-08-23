e963c79ead276cd275d701a4bf50bdf606e1a604
Ignacio <ignacio.martinez@ucb.edu.bo>
2023-08-23 10:24:57 -0400

    divisibles por 100 y no 400

--- src/bisiesto.js
+++ src/bisiesto.js
@@ -1,6 +1,8 @@
 function bisiesto(anio) {
     if (anio % 400 == 0)
         return true;
+    if (anio % 100 == 0 && anio % 400 != 0)
+        return false;
     return false;
 }
 
--- src/bisiesto.test.js
+++ src/bisiesto.test.js
@@ -4,4 +4,8 @@ describe("Bisiesto", () => {
   it("Anio bisiesto si es divisible por 400", () => {
     expect(bisiesto(1600)).toEqual(true);
   });
+
+  it("Anio bisiesto si es divisible por 100 pero no por 400", () => {
+    expect(bisiesto(1800)).toEqual(false);
+  });
 });
